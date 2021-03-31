b13284738ba5cb08199e8a111ad8134f7ce92d0a
Alejandra <macostaq94@gmail.com>
2021-03-31 16:23:40 -0500

    Sistema de rutas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import logo from './logo.svg';
 import './App.scss';
 import { DatePicker, Space } from 'antd';
+import {BrowserRouter as Router, Route, link} from 'react-router-dom';
 
 
 function App() {
+
   const test = (date)=>{
     console.log(date)
   }
   return (
+    <Router>
     <div className="App">
-      <h1> Web TuSemillero</h1>
+      <h1> Rutas</h1>
 
-      <DatePicker  onChange={test}/>
 
     </div>
+    </Router>
   );
 }
+
+function Home(){
+  return <h2> estamos en el home</h2>
+}
+
+function Contact(){
+  return <h2> estamos en el Contact</h2>
+}
+
+function User(){
+  return <h2> estamos en el User</h2>
+}
+
+
+
 
 export default App;
