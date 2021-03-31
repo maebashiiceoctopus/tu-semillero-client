022c44a06f704fce5dd7a8adc985aa65808a3075
Alejandra <macostaq94@gmail.com>
2021-03-31 18:44:17 -0500

    Bosquejo páginas base

--- src/App.js
+++ src/App.js
@@ -1,50 +1,22 @@
-import logo from "./logo.svg";
+import React  from 'react';
 import "./App.scss";
-import { DatePicker, Space } from "antd";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Admin from './pages/Admin-page/Admin';
+import Contact from './pages/Admin-page/Contact';
+import SignIn from './pages/Admin-page/SignIn';
+import Home from './pages/Home';
 
 function App() {
-  const test = (date) => {
-    console.log(date);
-  };
-  return (
-    <Router>
-      <div className="App">
-        <h1> Rutas</h1>
-
-        <Link to= '/'>Home</Link>
-        <br/>
-        <Link to= '/Contact'>Contact</Link>
-        <br/>
-        <Link to= '/Users'>Users</Link>
-
-
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/Contact" exact component={Contact} />
-          <Route exact path="/User" component={User} />
-          <Route component={Error404} />
-
-        </Switch>
-      </div>
-    </Router> 
-  );
+ return(
+   <div>
+     <h1>TuSemillero APP</h1>
+     <Admin/>
+     <SignIn/>
+     <Home/>
+     <Contact/>
+   </div>
+ )
 }
 
-function Home() {
-  return <h2> estamos en el home</h2>;
-}
 
-function Contact() {
-  return <h2> estamos en el Contact</h2>;
-}
-
-function User() {
-  return <h2> estamos en el User</h2>;
-}
-
-function Error404() {
-  return <h2> Error 404!!!!</h2>;
-}
 
 export default App;
---
+++ src/pages/Admin-page/SignIn.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+
+export default function SignIn(){
+    return(
+        <h1>Estamos en el login</h1>
+    );
+}
