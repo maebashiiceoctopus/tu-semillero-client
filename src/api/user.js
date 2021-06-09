f44049d8a2a1a9919dcfdf06e383d7402969c7e2
Alejandra <macostaq94@gmail.com>
2021-06-09 12:10:49 -0500

    endpoint conecction
    Se conecta el endpoint de registro de usuario al front y se generan las notificaciones de respuesta.

---
+++ src/api/user.js
@@ -0,0 +1,35 @@
+import { basePath, apiVersion } from "./config";
+
+export function signUpApi(data) {
+  const url = `${basePath}/${apiVersion}/sign-up`;
+  const params = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "content-type": "application/json",
+    },
+  };
+
+  return fetch(url, params)
+    .then((response) => {
+      return response.json();
+    })
+    .then((result) => {
+      if (result.user) {
+        return {
+          ok: true,
+          message: "El usuario se ha creado correctamente",
+        };
+      }
+      return {
+        ok: false,
+        message: result.message,
+      };
+    })
+    .catch((err) => {
+      return {
+        ok: false,
+        message: err.message,
+      };
+    });
+}
