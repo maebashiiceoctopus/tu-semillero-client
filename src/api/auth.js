968da553dd139b6cc6c953045ee6f00ea3107eb9
Alejandra <macostaq94@gmail.com>
2021-06-23 10:39:40 -0500

    obtener el access token mientras sea valido

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,26 @@
-import {basePath, apiVersion} from './config';
-import { ACCESS_TOKEN, REFRESH_TOKEN} from '../utils/constants';
-import jwtDecode from 'jwt-decode';
+import { basePath, apiVersion } from "./config";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../utils/constants";
+import jwtDecode from "jwt-decode";
 
-export function getAccessToken(){
-    return 'Toma el access token'
+export function getAccessToken() {
+  const accessToken = localStorage.getItem(ACCESS_TOKEN);
+
+  if (!accessToken || accessToken === "null") {
+    return null;
+  } else {
+  }
+
+ return willExpireToken(accessToken) ? null : accessToken;
+  
+}
+
+function willExpireToken(token) {
+  const seconds = 60;
+  const metaToken = jwtDecode(token);
+
+  const { exp } = metaToken;
+
+  const now = (Date.now() + seconds) / 1000;
+
+  return now > exp;
 }
