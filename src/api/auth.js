9b55ca7aa896a7e656c683cab337f7fbbcd00452
Alejandra <macostaq94@gmail.com>
2021-06-23 14:12:03 -0500

    petición de refresh token

--- src/api/auth.js
+++ src/api/auth.js
@@ -9,18 +9,48 @@ export function getAccessToken() {
     return null;
   }
 
- return willExpireToken(accessToken) ? null : accessToken;
-  
+  return willExpireToken(accessToken) ? null : accessToken;
 }
 
-export function getRefreshToken(){
-    const refreshToken= localStorage.getItem(REFRESH_TOKEN);
-    if(!refreshToken || refreshToken=== "null"){
+export function getRefreshToken() {
+  const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+  if (!refreshToken || refreshToken === "null") {
+    return null;
+  }
+  return willExpireToken(refreshToken) ? null : refreshToken;
+}
+
+export function refreshAccessToken(refreshToken) {
+  const url = `${basePath}/${apiVersion}/refresh-access-token`;
+  const bodyObj = {
+    refreshToken: refreshToken,
+  };
+  const params = {
+    method: "POST",
+    body: JSON.stringify(bodyObj),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  fetch(url, params)
+    .then((response) => {
+      if (response.status !== 200) {
         return null;
-    }
-    return willExpireToken(refreshToken) ? null : refreshToken;
-}
+      }
+      return response.json();
+    })
+    .then((result) => {
+        if(!result){
+            //to do desloguear usuario
 
+        }else{
+            const {accessToken, refreshToken}= result;
+            localStorage.setItem(ACCESS_TOKEN, accessToken);
+            localStorage.setItem(REFRESH_TOKEN, refreshToken);
+        }
+    });
+}
 
 function willExpireToken(token) {
   const seconds = 60;
