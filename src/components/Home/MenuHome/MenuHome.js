6566f663f8d17139a46af20e97c182edb67b131f
maebashiiceoctopus <macostaq94@gmail.com>
2021-10-17 22:53:57 -0500

    se ajusta responsive de home

--- src/components/Home/MenuHome/MenuHome.js
+++ src/components/Home/MenuHome/MenuHome.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Menu } from "antd";
 import { Link } from "react-router-dom";
 import SvisiLogo from "../../../assets/img/png/svisi-logo.png";
+import SvisiLogoMovil from "../../../assets/img/svg/svisi-movil.svg";
 import SocialMedia from "../SocialLinks/SocialMedia";
 
 import "./MenuHome.scss";
@@ -11,7 +12,9 @@ export default function MenuHome() {
     <Menu className="menu-home" mode="horizontal">
       <Menu.Item className="menu-home__logo">
         <Link to={"/"}>
-          <img src={SvisiLogo} alt="Svisi Semilleros" />
+          <img src={SvisiLogo} alt="Svisi Semilleros" className="logo-desk" />
+          <img src={SvisiLogoMovil} alt="logo svisi" className="logo-movil" />
+
         </Link>
       </Menu.Item>
       <Menu.Item className="menu-home__link">
